665001a66408a3fb957899d6a654ee67e0759f0b
Christa <jaysplaypen@gmail.com>
2024-10-09 17:00:19 -0700

    fix carousel finally

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,38 +1,42 @@
 import React from "react";
-// import OwlCarousel from "react-owl-carousel";
-// import { Link } from "react-router-dom";
-// import "owl.carousel/dist/assets/owl.carousel.css";
-// import "owl.carousel/dist/assets/owl.theme.default.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Trending = ({ blogs }) => {
-  const options = {
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 2,
-      },
-      600: {
-        items: 3,
-      },
-      1000: {
-        items: 4,
-      },
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 600 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 600, min: 400 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 400, min: 0 },
+      items: 1,
     },
   };
+
   return (
     <>
       <div>
         <div className="blog-heading text-start py-2 mb-4">Trending</div>
       </div>
-      {/* <OwlCarousel className="owl-theme" {...options}>
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={2000}
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+      >
         {blogs?.map((item) => (
           <div className="item px-2" key={item.id}>
-            <Link to={`/detail/${item.id}`}>
+            <a href={`/detail/${item.id}`}>
               <div className="trending-img-position">
                 <div className="trending-img-size">
                   <img
@@ -49,10 +53,10 @@ const Trending = ({ blogs }) => {
                   </div>
                 </div>
               </div>
-            </Link>
+            </a>
           </div>
         ))}
-      </OwlCarousel> */}
+      </Carousel>
     </>
   );
 };
