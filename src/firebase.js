d08382e29fab1aec3776d8eac05dff5f44598078
Christa <jaysplaypen@gmail.com>
2024-10-14 13:49:11 -0700

    fix .env

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,6 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    projectId: "fir-blog-ab578",
     storageBucket: "fir-blog-ab578.appspot.com",
     messagingSenderId: "588769144177",
     appId: "1:588769144177:web:e0ce27c0d7b1dbd608dad0",
